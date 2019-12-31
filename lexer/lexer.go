0bee52baf7ec94019c9bca52b3277feb0d7635f1
togatoga <togasakitogatoga@gmail.com>
2019-12-31 10:51:14 +0900

    Add a test fun to lexer_test.go and fix a bug

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -44,6 +44,9 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
 	}
 	l.readChar()
 	return tok
