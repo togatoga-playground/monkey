86f05c58e9a4a0a58b70399cb97779fa4301755a
togatoga <togasakitogatoga@gmail.com>
2019-12-31 10:44:27 +0900

    Impl NextToken

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -26,6 +26,27 @@ func (l *Lexer) readChar() {
 }
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	}
+	l.readChar()
+	return tok
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
