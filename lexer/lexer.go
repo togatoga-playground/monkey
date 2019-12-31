839663a6dfa3a5bd5fc96eaf8568eda24d3feb16
togatoga <togasakitogatoga@gmail.com>
2019-12-31 10:32:54 +0900

    Add lexer.go and token.go

---
+++ lexer/lexer.go
@@ -0,0 +1,33 @@
+package lexer
+
+import "github.com/togatoga/monkey/token"
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
