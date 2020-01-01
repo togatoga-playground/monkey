e589df43fd6864d67338483ccdae9427b8d0e3f2
togatoga <togasakitogatoga@gmail.com>
2020-01-02 05:30:09 +0900

    Add a return parser

--- ast/ast.go
+++ ast/ast.go
@@ -11,7 +11,7 @@ type Statement interface {
 	statementNode()
 }
 
-type Experssion interface {
+type Expression interface {
 	Node
 	expressionNode()
 }
@@ -39,8 +39,16 @@ func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
-	Value Experssion
+	Value Expression
 }
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
