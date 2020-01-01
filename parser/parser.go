9a7b895e8ee1df3e6021fcd52e91146356fffc26
togatoga <togasakitogatoga@gmail.com>
2020-01-01 22:05:49 +0900

    Add parser.go

---
+++ parser/parser.go
@@ -0,0 +1,29 @@
+package parser
+
+import (
+	"github.com/togatoga/monkey/ast"
+	"github.com/togatoga/monkey/lexer"
+	"github.com/togatoga/monkey/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
