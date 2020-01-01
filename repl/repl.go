6932500a6ebde6e10abf44d4ff5fac3b0e026c2f
togatoga <togasakitogatoga@gmail.com>
2020-01-01 20:49:38 +0900

    Add repl.go

---
+++ repl/repl.go
@@ -0,0 +1,30 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+
+	"io"
+
+	"github.com/togatoga/monkey/lexer"
+	"github.com/togatoga/monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
